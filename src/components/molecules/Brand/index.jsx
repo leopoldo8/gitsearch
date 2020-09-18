6cfc1ecc1e091f1168c971b0abd2d207442c7503
Rodrigo Haertel <rodrigo.haertel@yahoo.com.br>
2020-09-18 09:41:01 -0300

    FEAT: Brand molecule

---
+++ src/components/molecules/Brand/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Logo from '@components/atoms/Logo';
+
+import Title from './style';
+
+const Brand = () => (
+  <>
+    <Logo />
+    <Title>GitSearch</Title>
+  </>
+);
+
+export default Brand;
---
+++ src/components/molecules/Brand/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { Display } from '@assets/styles/typography';
+import { Black } from '@assets/styles/colors';
+
+export default styled.h1`
+  ${Display}
+  color: ${Black};
+  margin-top: 37px;
+`;
